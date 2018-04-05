a3dd72d08e4a865eb7fc44c0dd6e4bcc105aafaf
saileshkumarks <saileshkumar.ks@outlook.com>
2018-04-05 11:35:43 -0500

    Tracking Process adn Keystrokes
    Fall 2017 capstone Project

---
+++ capf17g5project1/capf17g5project1/trackingAndLogging.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace capf17g5project1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.timerKeyPress = new System.Windows.Forms.Timer(this.components);
+            this.timerLog = new System.Windows.Forms.Timer(this.components);
+            this.notifyPopup = new System.Windows.Forms.NotifyIcon(this.components);
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.timerProcessActivity = new System.Windows.Forms.Timer(this.components);
+            this.btnUploadToOSF = new System.Windows.Forms.Button();
+            this.lblOSFuploadStatus = new System.Windows.Forms.Label();
+            this.pBarUploadStatus = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 12);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(172, 42);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "START RECORDING";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(194, 12);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(63, 42);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "STOP";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // timerKeyPress
+            // 
+            this.timerKeyPress.Interval = 10;
+            this.timerKeyPress.Tick += new System.EventHandler(this.timerKeyPress_Tick);
+            // 
+            // timerLog
+            // 
+            this.timerLog.Interval = 6000;
+            this.timerLog.Tick += new System.EventHandler(this.timerLog_Tick);
+            // 
+            // notifyPopup
+            // 
+            this.notifyPopup.BalloonTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.notifyPopup.BalloonTipText = "Application has started";
+            this.notifyPopup.BalloonTipTitle = "KEY PRESS LOGGER";
+            this.notifyPopup.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyPopup.Icon")));
+            this.notifyPopup.Text = "Double-click to maximize";
+            this.notifyPopup.Visible = true;
+            this.notifyPopup.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyPopup_MouseDoubleClick);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(12, 89);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(115, 36);
+            this.btnUpload.TabIndex = 5;
+            this.btnUpload.Text = "Upload to AWS (S3)";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 129);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(60, 13);
+            this.lblStatus.TabIndex = 6;
+            this.lblStatus.Text = "Status: Idle";
+            // 
+            // timerProcessActivity
+            // 
+            this.timerProcessActivity.Interval = 3000;
+            this.timerProcessActivity.Tick += new System.EventHandler(this.timerProcessActivity_Tick);
+            // 
+            // btnUploadToOSF
+            // 
+            this.btnUploadToOSF.Location = new System.Drawing.Point(11, 150);
+            this.btnUploadToOSF.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUploadToOSF.Name = "btnUploadToOSF";
+            this.btnUploadToOSF.Size = new System.Drawing.Size(116, 36);
+            this.btnUploadToOSF.TabIndex = 7;
+            this.btnUploadToOSF.Text = "Upload to OSF";
+            this.btnUploadToOSF.UseVisualStyleBackColor = true;
+            this.btnUploadToOSF.Click += new System.EventHandler(this.btnUploadToOSF_Click);
+            // 
+            // lblOSFuploadStatus
+            // 
+            this.lblOSFuploadStatus.AutoSize = true;
+            this.lblOSFuploadStatus.Location = new System.Drawing.Point(11, 192);
+            this.lblOSFuploadStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.lblOSFuploadStatus.Name = "lblOSFuploadStatus";
+            this.lblOSFuploadStatus.Size = new System.Drawing.Size(60, 13);
+            this.lblOSFuploadStatus.TabIndex = 8;
+            this.lblOSFuploadStatus.Text = "Status: Idle";
+            // 
+            // pBarUploadStatus
+            // 
+            this.pBarUploadStatus.Location = new System.Drawing.Point(15, 224);
+            this.pBarUploadStatus.Name = "pBarUploadStatus";
+            this.pBarUploadStatus.Size = new System.Drawing.Size(211, 23);
+            this.pBarUploadStatus.TabIndex = 9;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(265, 266);
+            this.Controls.Add(this.pBarUploadStatus);
+            this.Controls.Add(this.lblOSFuploadStatus);
+            this.Controls.Add(this.btnUploadToOSF);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer timerKeyPress;
+        private System.Windows.Forms.Timer timerLog;
+        private System.Windows.Forms.NotifyIcon notifyPopup;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Timer timerProcessActivity;
+        private System.Windows.Forms.Button btnUploadToOSF;
+        private System.Windows.Forms.Label lblOSFuploadStatus;
+        private System.Windows.Forms.ProgressBar pBarUploadStatus;
+    }
+}
+
---
+++ capf17g5project1/capf17g5project1/trackingAndLogging.cs
@@ -0,0 +1,316 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using System.Diagnostics;
+using Amazon.S3;
+using Amazon.S3.Transfer;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace capf17g5project1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "TPK";
+            btnStop.Enabled = false;
+            lblStatus.Visible = false;
+            lblOSFuploadStatus.Visible = false;
+            /*Progress Bar*/
+            pBarUploadStatus.Visible = false;
+            pBarUploadStatus.Minimum = 0;
+            pBarUploadStatus.Maximum = 100;
+        }
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(Keys vKey);
+
+        [DllImport("User32.dll")]
+        private static extern short GetAsyncKeyState(System.Int32 vKey);
+
+        StringBuilder keyPressBuffer;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            keyPressBuffer = new StringBuilder();
+        }
+
+        List<string> listCurrentProcess = new List<string>();
+        List<string> listOld = new List<string>();
+        List<String> listClosedProcess = new List<string>();
+
+        #region LOG File Creation and START button code
+
+        void CreateLog(string logFileName)
+        {
+            try
+            {
+                StreamWriter writer = new StreamWriter(logFileName, true); //append log to file
+                writer.Write(keyPressBuffer.ToString());
+                writer.Close();
+                keyPressBuffer.Clear(); //reset buffer
+            }
+            catch { }
+        }
+
+
+        private void timerLog_Tick(object sender, EventArgs e)
+        {
+            int flagFileExists = 0;
+            if (File.Exists(@"c:\logKeyPress.csv"))
+            {
+                flagFileExists = 1;
+            }
+            else
+            {
+                flagFileExists = 0;
+            }
+            CreateLog(@"c:\logKeyPress.csv");
+
+            //adding header only if file doesn't exist
+            if (flagFileExists == 0)
+            {
+                /*Log file formatting in Header*/
+                //adding Header
+                keyPressBuffer.Append("Code, Timestamp, Details,");
+                //adding Code Legend
+                keyPressBuffer.Append("Code => 0-Keypress|1-Applicatioons|2-MoususeClick");
+                keyPressBuffer.Append("\n"); //new row                
+            }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            timerKeyPress.Enabled = false;
+            timerLog.Enabled = false;
+            btnStart.Enabled = true;
+            btnStop.Enabled = false;
+        }
+
+        private void notifyPopup_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            this.Show();
+        }
+
+        #endregion
+
+        #region KeyPress Tracking / Logging
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            timerProcessActivity.Enabled = true;
+            listCurrentProcess.Clear();
+            timerKeyPress.Enabled = true;
+            timerLog.Enabled = true;
+            btnStart.Enabled = false;
+            btnStop.Enabled = true;
+            Form1 objForm1 = new Form1();
+            objForm1.WindowState = FormWindowState.Minimized;
+            notifyPopup.ShowBalloonTip(3500);
+            this.Hide();
+        }
+
+        private void timerKeyPress_Tick(object sender, EventArgs e)
+        {
+            int typeOfEvent = 0;
+
+            foreach (System.Int32 i in Enum.GetValues(typeof(Keys)))
+            {
+                if (GetAsyncKeyState(i) == -32767)
+                {
+                    if ((Enum.GetName(typeof(Keys), i) == "LButton") || (Enum.GetName(typeof(Keys), i) == "MButton") || (Enum.GetName(typeof(Keys), i) == "RButton"))
+                    {
+                        typeOfEvent = 2; //Mouse Clicks
+                    }
+                    else
+                    {
+                        typeOfEvent = 0; //Keyboard key press
+                    }
+                    keyPressBuffer.Append(typeOfEvent);
+                    keyPressBuffer.Append(",");
+                    keyPressBuffer.Append(DateTime.Now.ToString("MMM/dd/yyyy hh:mm:ss:fff")); //or en-US
+                    keyPressBuffer.Append(","); //new column
+                    keyPressBuffer.Append(Enum.GetName(typeof(Keys), i));
+                    keyPressBuffer.Append("\n"); //new row
+                }
+            }
+        }
+
+        #endregion       
+
+        #region Process Tracking / Logging
+
+        private void timerProcessActivity_Tick(object sender, EventArgs e)
+        {
+            Process[] processlist = Process.GetProcesses();
+            int typeOfEvent = 1; //process
+
+            if (listCurrentProcess.Count != 0)
+            {
+                listOld.Clear();
+                listOld = new List<string>(listCurrentProcess);
+                listCurrentProcess.Clear();
+            }
+
+            foreach (Process p in processlist)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle))
+                {
+                    listCurrentProcess.Add(p.MainWindowTitle);
+                }
+            }
+
+            if (listClosedProcess.Count != 0)
+            {
+                listClosedProcess.Clear();
+            }
+
+            listClosedProcess = new List<string>(listOld.Except(listCurrentProcess)); //Get closed applications
+            int lengthListClosedProcess = listClosedProcess.Count();
+            if (lengthListClosedProcess >= 1) //print closed processes
+            {
+                foreach (String s in listClosedProcess)
+                {
+                    keyPressBuffer.Append(typeOfEvent);
+                    keyPressBuffer.Append(",");
+                    keyPressBuffer.Append(DateTime.Now.ToString("MMM/dd/yyyy hh:mm:ss")); //or en-US
+                    keyPressBuffer.Append(","); //new column
+                    keyPressBuffer.Append("CLOSED --> " + s.ToString());
+                    keyPressBuffer.Append("\n"); //new row
+                }
+            }
+
+            int lengthListCurrentProcess = listCurrentProcess.Count();
+            foreach (String s in listCurrentProcess) //print open processes
+            {
+                keyPressBuffer.Append(typeOfEvent);
+                keyPressBuffer.Append(",");
+                keyPressBuffer.Append(DateTime.Now.ToString("MMM/dd/yyyy hh:mm:ss")); //or en-US
+                keyPressBuffer.Append(","); //new column
+                keyPressBuffer.Append("OPEN --> " + s.ToString());
+                keyPressBuffer.Append("\n"); //new row
+            }
+        }
+
+        #endregion
+
+        #region Upload to AWS
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            lblStatus.Visible = true;
+            pBarUploadStatus.Value = 5;
+            pBarUploadStatus.Visible = true;
+            lblStatus.Text = "Status: UPLOADING....";
+            uploadevent();
+        }
+
+        private void uploadevent()
+        {
+            string filePath = @"c:\logKeyPress.csv";
+            string existingBucketName = "capf17g5"; //Created manually 
+            var accessKey = "AKIAIGBVXNNLMB4OPC6A";
+            var secretKey = "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";
+
+            try
+            {
+                TransferUtility fileTransferUtility = new
+                    TransferUtility(new AmazonS3Client(accessKey, secretKey, Amazon.RegionEndpoint.USEast1));
+                /* Upload the file(file name is taken as the object key name). */
+
+                try
+                {
+                    pBarUploadStatus.Value = 25;
+                    fileTransferUtility.Upload(filePath, existingBucketName);
+                }
+                catch
+                {
+                    MessageBox.Show("File not found");
+                    pBarUploadStatus.Value = 10;
+                    lblStatus.Text = "Status: Idle";
+                }
+
+                pBarUploadStatus.Value = 50;
+                /* Set the file storage class and the sharing type*/
+                TransferUtilityUploadRequest fileTransferUtilityRequest = new TransferUtilityUploadRequest
+                {
+                    BucketName = existingBucketName,
+                    FilePath = filePath,
+                    StorageClass = S3StorageClass.Standard,
+                    CannedACL = S3CannedACL.PublicReadWrite,
+                    ContentType = "text/csv", //video/x-mp4 (MMIE type for video)
+                };
+                try
+                {
+                    pBarUploadStatus.Value = 75;
+                    fileTransferUtility.Upload(fileTransferUtilityRequest);
+                    lblStatus.Text = "Status: UPLOADED";
+                    pBarUploadStatus.Value = 100;
+                }
+                catch
+                {
+                    lblStatus.Text = "Failed to upload the file";
+                    pBarUploadStatus.Value = 10;
+                }
+            }
+            catch (AmazonS3Exception)
+            {
+                lblStatus.Text = "Failed to upload the file";
+                pBarUploadStatus.Value = 10;
+            }
+        }
+
+        #endregion
+
+        #region Upload to OSF
+
+        private void btnUploadToOSF_Click(object sender, EventArgs e)
+        {
+            lblOSFuploadStatus.Visible = true;
+            pBarUploadStatus.Value = 5;
+            pBarUploadStatus.Visible = true;
+            lblOSFuploadStatus.Text = "Status: UPLOADING....";
+            uploadFileToOSF();
+        }
+
+        public void uploadFileToOSF()
+        {
+            string location = "pn24m";
+            string filePath = @"c:\logKeyPress.csv";
+            string nameTheFile = "logKeyPress_" + DateTime.Now.ToString("MMMddyyyyHHmmss") + ".csv";
+            string authentication = "Bearer RUhnM3Je768HzmWEfqSIh7FP7QXK8qrXYI2j3NRJ1daiPOpMBxckhkYBWUR9105wFbexiU";//Navya
+
+            try
+            {
+                ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls11;
+                //Initiate a new web client
+                WebClient wc = new WebClient();
+                wc.Headers.Add("Authorization", authentication);
+                wc.Headers[HttpRequestHeader.ContentType] = "application/vnd.api+json";
+                //track upload progress
+                wc.UploadProgressChanged += new UploadProgressChangedEventHandler(UploadProgressCallback);
+                wc.UploadFile(new Uri("https://files.osf.io/v1/resources/" +
+                    location + "/providers/osfstorage/?name=" + nameTheFile + "&kind=file"), "PUT", filePath);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                lblOSFuploadStatus.Text = "Upload not successful";
+                pBarUploadStatus.Value = 10;
+            }
+
+            lblOSFuploadStatus.Text = "Uploaded to OSF Successfully!";
+            pBarUploadStatus.Value = 100;
+        }
+
+        private void UploadProgressCallback(object sender, UploadProgressChangedEventArgs e) =>
+            pBarUploadStatus.Value = e.ProgressPercentage;
+
+        #endregion
+
+    }
+}
